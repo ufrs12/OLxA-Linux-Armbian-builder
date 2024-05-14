5191cbfb0d8cd786db732f47d51396a7e7389cd8
gantres <137826620+Gantres1@users.noreply.github.com>
2024-05-14 21:15:38 +0700

    1

--- src/components/Installer/Installer.tsx
+++ src/components/Installer/Installer.tsx
@@ -4,22 +4,24 @@ import "./Installer.css"
 import { Link, Route, Routes } from "react-router-dom";
 
 export default function Installer (){
-  <div className='installer-container'>
-    <div className='installer-options'>
-      <Link to={"/"}>Базовые настройки</Link>
-      <Link to={"/"}>OLIA</Link>
-      <Link to={"/"}>OLHA</Link>
-      <Link to={"/"}>OLTA</Link>
-      <Link to={"parameters"}>Параметры</Link>
-      
-      <button>Получить сборщик</button>
+  return(
+    <div className='installer-container'>
+      <div className='installer-options'>
+        <Link to={"/"}>Базовые настройки</Link>
+        <Link to={"/"}>OLIA</Link>
+        <Link to={"/"}>OLHA</Link>
+        <Link to={"/"}>OLTA</Link>
+        <Link to={"parameters"}>Параметры</Link>
+        
+        <button>Получить сборщик</button>
+      </div>
+      <Routes>
+        <Route path="/" element={<BasicSettings />}></Route>
+        <Route path="*" element={<BasicSettings />}></Route>
+        <Route path="*" element={<BasicSettings />}></Route>
+        <Route path="*" element={<BasicSettings />}></Route>
+        <Route path="parameters" element={<Parameters />}></Route>
+      </Routes>
     </div>
-    <Routes>
-      <Route path="/" element={<BasicSettings />}></Route>
-      <Route path="*" element={<BasicSettings />}></Route>
-      <Route path="*" element={<BasicSettings />}></Route>
-      <Route path="*" element={<BasicSettings />}></Route>
-      <Route path="parameters" element={<Parameters />}></Route>
-    </Routes>
-  </div>
+  )
 }
