3f4d8b5080d33b5cbbaa8827f3c4345f218b0dd7
gantres <137826620+Gantres1@users.noreply.github.com>
2024-05-15 18:16:50 +0700

    1

--- src/components/Installer/Installer.tsx
+++ src/components/Installer/Installer.tsx
@@ -8,9 +8,9 @@ export default function Installer (){
     <div className='installer-container'>
       <div className='installer-options'>
         <Link to={"/"} className="options-item">Базовые настройки</Link>
-        <Link to={"/"} className="options-item">OLIA</Link>
-        <Link to={"/"} className="options-item">OLHA</Link>
-        <Link to={"/"} className="options-item">OLTA</Link>
+        <Link to={"olia"} className="options-item">OLIA</Link>
+        <Link to={"olha"} className="options-item">OLHA</Link>
+        <Link to={"olta"} className="options-item">OLTA</Link>
         <Link to={"parameters"} className="options-item">Параметры</Link>
         
         <button className="installer-btn">Завершить</button>
@@ -18,9 +18,9 @@ export default function Installer (){
       <div className="installer-form">
         <Routes>
           <Route path="/" element={<BasicSettings />}></Route>
-          <Route path="*" element={<BasicSettings />}></Route>
-          <Route path="*" element={<BasicSettings />}></Route>
-          <Route path="*" element={<BasicSettings />}></Route>
+          <Route path="olia" element={<BasicSettings />}></Route>
+          <Route path="olha" element={<BasicSettings />}></Route>
+          <Route path="olta" element={<BasicSettings />}></Route>
           <Route path="parameters" element={<Parameters />}></Route>
         </Routes>
       </div>
