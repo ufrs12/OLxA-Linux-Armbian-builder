69d9b52d3c9bcf99a89cbf7a73a7a3b8044433bb
gantres <137826620+Gantres1@users.noreply.github.com>
2024-05-17 21:18:35 +0700

    1

--- src/models/Armbian.ts
+++ src/models/Armbian.ts
@@ -1,11 +1,28 @@
-import boardsData from "../assets/armdata/v23/boards/boards_all.json"
+import boardsData from "../armdata/v23/boards/boards_all.json"
 
-export interface Board {
+interface Board {
   name: string;
   supp: string;
   text: string;
   kernels: string[];
 }
+
+// enum Supp {
+//   Conf = ".conf",
+//   Csc = ".csc",
+//   EOS = ".eos",
+//   Md = ".md",
+//   Tvb = ".tvb",
+//   Wip = ".wip",
+// }
+
+// enum Kernel {
+//   Collabora = "collabora",
+//   Current = "current",
+//   Edge = "edge",
+//   Legacy = "legacy",
+//   Vendor = "vendor",
+// }
 
 export class Armbian {
   armVersion: string;
