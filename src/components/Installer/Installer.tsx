a197171a27b112848c3e31f2abdfd0005a4da83c
ufrs12 <ufrs12@yandex.ru>
2024-05-19 00:31:22 +0300

    1

--- src/components/Installer/Installer.tsx
+++ src/components/Installer/Installer.tsx
@@ -3,9 +3,11 @@ import BasicSettings from "../BasicSettings/BasicSettings";
 import Parameters from "../Parameters/Parameters";
 import { Link, Route, Routes } from "react-router-dom";
 import { Armbian } from "../../models/Armbian";
-import React from "react";
+import { Build } from "../../models/Build";
 
-export const ArmbianContext = React.createContext({});
+//export const ArmbianContext = React.createContext(new Armbian("24", ["1", "2"]));
+export const armbian = new Armbian();
+export var build = new Build(armbian.boards[0].name, armbian.boards[0].kernels[0]);
 
 export default function Installer (){
   return(
@@ -15,12 +17,10 @@ export default function Installer (){
         <Link to={"olia"} className="options-item">OLIA</Link>
         <Link to={"olha"} className="options-item">OLHA</Link>
         <Link to={"olta"} className="options-item">OLTA</Link>
-        <Link to={"parameters"} className="options-item">Параметры</Link>
-        
+        <Link to={"parameters"} className="options-item">Параметры</Link> 
         <button className="installer-btn">Завершить</button>
       </div>
       <div className="installer-form">
-        <ArmbianContext.Provider value={new Armbian("24", ["1", "2"])}>
           <Routes>
             <Route path="/" element={<BasicSettings />}></Route>
             <Route path="olia" element={<BasicSettings />}></Route>
@@ -28,7 +28,6 @@ export default function Installer (){
             <Route path="olta" element={<BasicSettings />}></Route>
             <Route path="parameters" element={<Parameters />}></Route>
           </Routes>
-        </ArmbianContext.Provider>
       </div>
     </div>
   )
