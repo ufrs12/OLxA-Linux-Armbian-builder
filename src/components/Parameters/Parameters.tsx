de710d1929b7d242eafb26d51e545bcc13be6652
gantres <137826620+Gantres1@users.noreply.github.com>
2024-05-21 21:13:57 +0700

    1

--- src/components/Parameters/Parameters.tsx
+++ src/components/Parameters/Parameters.tsx
@@ -24,41 +24,23 @@ export default function Parameters () {
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement>, 
     field: any,
-    mask: string
   ) => {
-    const input = e.target;
-    const inputNumbersValue = input.value.replace(/\D/g, '');
-    let formattedInputValue = "";
+    // const input = e.target;
+    // const parts = input.value.split('.'); // Ограничиваем длину каждой части до 3 цифр
+    // if (parts.length > 4) {return}; // Не более 4 частей
+    // const numbers = parts.map(part => {
+    //   const num = parseInt(part, 10);
+    //   return isNaN(num) ? 0 : num > 255 ? 255 : num; 
+    // });
 
-    formattedInputValue += inputNumbersValue.substring(0, 3);
-    if (inputNumbersValue.length > 3){
-      formattedInputValue += "." + inputNumbersValue.substring(3,6);
-    }
-    if (mask === "xxx.xxx.x.x"){
-      if (inputNumbersValue.length > 6){
-        formattedInputValue += "." + inputNumbersValue.substring(6,7);
-      } if (inputNumbersValue.length > 7){
-        formattedInputValue += "." + inputNumbersValue.substring(7,8);
-      }
-    } else if (mask === "xxx.xxx.xxx.x"){
-      if (inputNumbersValue.length > 6){
-        formattedInputValue += "." + inputNumbersValue.substring(6,9);
-      } if (inputNumbersValue.length > 9){
-        formattedInputValue += "." + inputNumbersValue.substring(9,10);
-      }
-    }
-
-    e.target.value = formattedInputValue;
-    
-    if ((inputNumbersValue.length === 8 && mask === "xxx.xxx.x.x") || (inputNumbersValue.length === 10 && mask === "xxx.xxx.xxx.x")){
-      field.onChange(e.target.value);
-      setValue(field.name, e.target.value);
-    }
+    field.onChange(e.target.value);
+    setValue(field.name, e.target.value);
+    console.log(field.name);
   }
 
   const InputMask: React.FC <(
-    { name: string; mask: string }
-  )> = ({ name, mask }) => {
+    { name: string }
+  )> = ({ name }) => {
     return(
       <Controller 
         name={name as 'lanip' | 'lansubnet' | 'langate'}
@@ -66,10 +48,7 @@ export default function Parameters () {
         render={({ field }) => (
           <input 
             type="text" 
-            placeholder={field.value} 
-            defaultValue={field.value}
-            maxLength={mask.length} 
-            onChange={(e) => handleInputChange(e, field, mask)} 
+            onChange={(e) => handleInputChange(e, field)} 
           />
         )}
       />
@@ -77,15 +56,15 @@ export default function Parameters () {
   }
 
   const fields = [
-    { name: "lanip", mask: "xxx.xxx.x.x" },
-    { name: "lansubnet", mask: "xxx.xxx.xxx.x" },
-    { name: "langate", mask: "xxx.xxx.x.x" }
+    { name: "lanip" },
+    { name: "lansubnet" },
+    { name: "langate" }
   ]
 
   return(
     <form>
       {fields.map((field) => (
-        <InputMask key={field.name} name={field.name} mask={field.mask} />
+        <InputMask key={field.name} name={field.name} />
       ))}
     </form>
   )
