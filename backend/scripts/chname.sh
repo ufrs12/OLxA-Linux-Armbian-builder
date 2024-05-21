33e6d723ab959e37ae2838f723e19aaf4b70a161
ufrs12 <ufrs12@yandex.ru>
2024-05-21 22:23:45 +0300

    1111

--- backend/scripts/chname.sh
+++ backend/scripts/chname.sh
@@ -3,6 +3,7 @@
 # выполняем айпи линк | берем строку с превым вхождением "линк/эзер" | берем с 16-го по 33-го символ
 mac=$(ip link | grep "link/ether" -m 1 | cut -c16-33)
 # создаем строку для изменения имени сетевого интерфейса
+#KERNEL=="ens192", ATTR{address}=="00:50:56:01:27:c2", NAME="wan"
 ch=$(cat <<EndOfMessage
 SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="$mac", ATTR{dev_id}=="0x0", ATTR{type}=="1", NAME="eth0"
 EndOfMessage
