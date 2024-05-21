a10a3c84be5963f72e77011935c43ab738f0ab7b
ufrs12 <ufrs12@yandex.ru>
2024-05-21 21:20:40 +0300

    1111

--- backend/scripts/chname.sh
+++ backend/scripts/chname.sh
@@ -3,8 +3,10 @@
 # выполняем айпи линк | берем строку с превым вхождением "линк/эзер" | берем с 16-го по 33-го символ
 mac=$(ip link | grep "link/ether" -m 1 | cut -c16-33)
 # создаем строку для изменения имени сетевого интерфейса
-ch= $(cat <<EndOfMessage SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="$mac", ATTR{dev_id}=="0x0", ATTR{type}=="1", NAME="eth0" EndOfMessage)
+ch=$(cat <<EndOfMessage
+SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="$mac", ATTR{dev_id}=="0x0", ATTR{type}=="1", NAME="eth0"
+EndOfMessage
+)
 echo $ch
-echo $ch
-echo $ch
+#добавляем правило для именования интерфейса
 echo $ch >> /etc/udev/rules.d/70-persistent-net.rules
