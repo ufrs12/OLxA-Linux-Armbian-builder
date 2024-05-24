57b555cec72b963efee61e5644cdf5548f416ae2
ufrs12 <ufrs12@yandex.ru>
2024-05-24 09:02:27 +0300

    1

--- src/components/Installer/FilesCreator/ReBuildContent.ts
+++ src/components/Installer/FilesCreator/ReBuildContent.ts
@@ -10,8 +10,8 @@ n=$(grep -n ';;' ./build/userpatches/customize-image.sh -m 1 | sed "s/:.*//")
 nn=$((n - 1))
 echo ` + "${nn}" + `
 sed -i.bak "1,` + "${nn}" + `d" ./build/userpatches/customize-image.sh
-cat ./Загрузки/builder/userpatches/body-customize-image.sh ./build/userpatches/customize-image.sh> ./Загрузки/builder/userpatches/customize-image.sh
-mv ./Загрузки/builder/userpatches/customize-image.sh ./build/userpatches/
+cat ./builder/userpatches/body-customize-image.sh ./build/userpatches/customize-image.sh> ./builder/userpatches/customize-image.sh
+mv ./builder/userpatches/customize-image.sh ./build/userpatches/
 
 
 # 3. Run Armbian build with options first time
